0f89e408c8db244d58ba18c88c35cf6470dc9950
Timmy Jose <zoltan.jose@gmail.com>
2023-09-07 13:54:19 +0530

    01_my_first_macro

--- exercises/01_my_first_macro/main.rs
+++ exercises/01_my_first_macro/main.rs
@@ -7,6 +7,11 @@ fn show_output() {
 
 // TODO: create `show_output!()` macro.
 
+macro_rules! show_output {
+    () => {
+        show_output()
+    };
+}
 ////////// DO NOT CHANGE BELOW HERE /////////
 
 fn main() {
